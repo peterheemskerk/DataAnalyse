58f3811820b8f9ad6df9ef27a4d08bf7464bc0f1
pasqualjl <pasqualjl@me.com>
2018-06-13 11:40:35 +0200

    Cleaning up GitHub

---
+++ Programmas/KNMI.py
@@ -0,0 +1,118 @@
+# This program produces two dictionaries that make retrieving the information
+# about the stations and the attributes smoother in programs that import this.
+# It also contains a useful helper function that can calculate the number of
+# days between 1901/01/01 and any given date after that.
+
+PATH = "../KNMI.txt"
+
+_START_CHAR_STN = 690
+_START_CHAR_ATT = 3554
+
+_YEAR0 = 1901
+_MONTH0 = 1
+_DAY0 = 1
+
+stn = dict()
+attributes = dict()
+
+# The station class stores some information about each station
+class Station:
+
+    all_num = []
+
+    def __init__(self, num, name, lon, lat, alt):
+        self.num = num
+        self.name = name
+        self.lon = lon
+        self.lat = lat
+        self.alt = alt
+
+        Station.all_num.append(self.num)
+
+    def __str__(self):
+        return "STN " + str(self.num) + ":\"" + self.name + "\""
+
+
+# Read the name and explanation of the attributes and put them in the dictionary
+def _read_attributes(filename):
+    with open(filename) as text:
+        text.seek(_START_CHAR_ATT)
+
+        line = text.readline()[2:-1]
+        while line:
+            name = line[:line.index(' ')]
+            explanation = line[line.index("=")+2:-2]
+
+            index = explanation.find("Zie http://")
+            if index != -1:
+                explanation = explanation[:index - 2]
+
+            attributes[name] = explanation
+
+            line = text.readline()[2:-1]
+
+
+# Read the details about the station and but them in the dictionary.
+def _read_stations(filename):
+    with open(filename) as text:
+        text.seek(_START_CHAR_STN)
+
+        line = text.readline()[2:-1]
+        while line:
+            num = int(line[:line.index(':')])
+
+            line = line[line.index(':')+1:].lstrip()
+            lon = float(line[:line.index(' ')])
+
+            line = line[line.index(' ')+1:].lstrip()
+            lat = float(line[:line.index(' ')])
+
+            line = line[line.index(' ')+1:].lstrip()
+            alt = float(line[:line.index(' ')])
+
+            name = line[line.rindex(' ')+1:]
+
+            station = Station(num, name, lon, lat, alt)
+            stn[num] = station
+            stn[name] = station
+
+            line = text.readline()[2:-1]
+
+
+def get_t(time):
+    """Returns the number of days between 'time' and 1901/01/01.\n
+    time = any date after 1901/01/01"""
+
+    year = time // 10000
+    d_year = year - _YEAR0
+    time -= year * 10000
+    d_month = time // 100 - _MONTH0
+    d_day = time % 100 - _DAY0
+
+    t = d_year * 365 + d_year // 4
+    t += d_month * 31 - d_month // 2
+    if d_month >= 2:
+        t -= 2
+
+        if year % 4 == 0:
+            t += 1
+
+        if d_month >= 8:
+            t += 1
+
+    return t + d_day
+
+
+def _main():
+    global PATH
+    try:
+        open(PATH)
+    except IOError:
+        PATH = input("Type the relative path to your KNMI text file: ")
+
+    filename = PATH
+    _read_stations(filename)
+    _read_attributes(filename)
+
+
+_main()
---
+++ Programmas/Preprocessing.py
@@ -0,0 +1,111 @@
+# For this program you need to have your KNMI txt file downloaded. This program
+# will make 2 additional csv files:
+# The first will be the .txt file, but converted to .csv
+# The second will be the same as the first, but with all attributes and entries
+# removed that don't occur often enough.
+
+import numpy as np
+import pandas as pd
+
+import KNMI
+
+START_CHAR = 6548
+ATR_THRESH = 0.6
+ENT_THRESH = 0.6
+
+
+# Write all entries to a new file that only takes entries and atributes that
+# occur a certain percentage of the time.
+def reduce_entries(filename, atributes):
+    reduced_filename = filename[:filename.rindex('.')] + "_reduced.csv"
+    n_lines = 0
+    n_atr = len(atributes) - 1
+
+    with open(filename) as raw:
+        with open(reduced_filename, 'w') as reduced:
+            arr = np.array(raw.readline().split(','))
+            arr = arr[atributes]
+            reduced.write(','.join(arr))
+
+            line = raw.readline()
+            while line:
+                arr = np.array(line.split(','))
+                arr = arr[atributes]
+
+                if ((arr != "").sum() - 1) / n_atr > ENT_THRESH:
+                    n_lines += 1
+                    reduced.write(",".join(arr))
+
+                line = raw.readline()
+
+    return reduced_filename, n_lines
+
+
+# Return an array of all the atributes that occur in at least ATR_THRESH
+# percent of the entries.
+def find_valid_atributes(filename):
+    atributes = []
+
+    with open(filename) as knmi:
+        knmi.readline()
+
+        line = knmi.readline()
+        atributes = np.zeros(len(line.split(',')))
+        while line:
+            atributes[np.array(line.split(',')) != ''] += 1
+            line = knmi.readline()
+
+    atributes /= atributes[-1]
+    return np.arange(len(atributes))[atributes > ATR_THRESH]
+
+
+# Convert the .txt file to a .csv file
+def to_csv(filename):
+    csv_filename = filename[:filename.rindex('.')] + ".csv"
+    n_lines = 0
+
+    with open(filename) as knmi:
+        with open(csv_filename, 'w') as csv:
+            knmi.seek(START_CHAR)
+            csv.write(knmi.readline().replace(' ', ''))
+
+            knmi.readline()
+            line = knmi.readline()
+            while line:
+                n_lines += 1
+                line = line.replace("\t", "")
+                line = line.replace(" ", "")
+                csv.write(line)
+
+                line = knmi.readline()
+
+    return csv_filename, n_lines
+
+
+def main():
+    filename = KNMI.PATH
+
+    print("Converting", filename, "to a .csv file...", end=' ', flush=True)
+    csv_filename, n_lines = to_csv(filename)
+    print("Done.\nThe new file is called", csv_filename, "and has", n_lines,
+          "entries.")
+
+    print("Find out which attributes have data in at least", ATR_THRESH * 100,
+          "percent of the entries...", end=' ', flush=True)
+    atributes = find_valid_atributes(csv_filename)
+    print("Done.\nThe valid attributes are:", atributes)
+
+    print("Remove invalid attributes and entries that don't have at least",
+          ENT_THRESH * 100, "percent of the attributes...", end=' ', flush=True)
+    reduced_filename, n_lines = reduce_entries(csv_filename, atributes)
+    print("Done.\nThe new file is called", reduced_filename, "and has", n_lines,
+          "entries.")
+    
+    # Fills in NAN values with mean of the column
+
+    df = pd.read_csv(reduced_filename)
+    df = df.fillna(df.mean())
+    print('Filling in the gaps')
+    df.to_csv('FINAL_CSV_FILE.csv')
+
+main()
