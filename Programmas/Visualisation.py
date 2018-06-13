0bef2231f2828044074e58f42ed7e4297defbb43
peterheemskerk <peter.heemskerk@planet.nl>
2018-06-13 15:42:13 +0200

    .

--- Programmas/Visualisation.py
+++ Programmas/Visualisation.py
@@ -58,6 +58,64 @@ def attributes_over_time(df, stn, atts, start=19010101, end=20991231):
     return att_arr[mask], date_arr[mask]
 
 
+def get_attribute_all_stn(df, att, start=19010101, end=20991231):
+    """Peter. Returns a tuple of the values of the given attribute and the time.\n
+    df = pandas.DataFrame, all the data extracted from your csv file\n
+    all stations.\n
+    att = attribute\n
+    start = start date\n
+    end = end date"""
+
+    data = df.loc[(df.YYYYMMDD >= start)
+                  & (df.YYYYMMDD <= end), ["YYYYMMDD", att]].values
+    return data[:, 1], data[:, 0]
+
+
+
+def avg_over_year_all_stn(df, att, start=19010101, end=20991231):
+    """Peter. Returns a tuple of the values of the given attribute and the years.\n
+    df = pandas.DataFrame, all the data extracted from your csv file.\n
+    average over all stations.\n
+    att = attribute\n
+    start = start date\n
+    end = end date"""
+
+    att_arr, t_arr = get_attribute_all_stn(df, att, start, end)
+
+    years = []
+    att_avg = []
+    for year in range(start // 10000, end // 10000 + 1):
+        att = att_arr[t_arr // 10000 == year]
+
+        if len(att) > 0:
+            years.append(year)
+            att_avg.append(np.nanmean(att))		# used nanmean to ignore nan values
+
+    return np.array(att_avg), np.array(years)
+
+
+def avg_over_month_all_stn(df, att, start=19010101, end=20991231):
+    """Peter. Returns a tuple of the values of the given attribute and the months.\n
+    df = pandas.DataFrame, all the data extracted from your csv file.\n
+    average over all stations.\n
+    att = attribute\n
+    start = start date\n
+    end = end date"""
+
+    att_arr, t_arr = get_attribute_all_stn(df, att, start, end)
+
+    months = []
+    att_avg = []
+    for month in range(1, 12):
+        att = att_arr[(t_arr // 100) % 100 == month]
+
+        if len(att) > 0:
+            months.append(month)
+            att_avg.append(np.nanmean(att))		# used nanmean to ignore nan values
+
+    return np.array(att_avg), np.array(months)
+
+
 def avg_over_att(df, stn, xatt, yatt, start=19010101, end=20991231):
     """Returns a tuple of the values of the two given attribute.\n
     df = pandas.DataFrame, all the data extracted from your csv file.\n
@@ -437,6 +495,38 @@ def plot_measure_year(df, stn_arr, att=None, start=19010101, end=20991231,
 
     plt.show()
 
+def plot_att_month(df, stn_arr, att, start=19010101, end=20991231, markers=MARKERS):
+    """Plot the average value of an attribute over a year from multiple stations
+    \ndf = pandas.DataFrame, all data extracted from your csv file.\n
+    stn_arr = station array, an array of the numbers or names of the stations.\n
+    att = attribute\n
+    start = start date\n
+    end = end date\n
+    markers = a list of the shape and color of the markers in the plot."""
+    
+    if stn_arr == []:
+        att_arr, months = avg_over_month_all_stn(df, att, start, end)
+        plt.plot(months, att_arr, markers[0], label = 'all stations')
+    else:
+        print('not implemented yet')
+        '''
+        for i, stn in enumerate(stn_arr):
+            att_arr, years = avg_over_year(df, stn, att, start, end)
+            plt.plot(years, att_arr, markers[i], label=KNMI.stn[stn].name)
+        '''
+    plt.xlabel("Maanden")
+    plt.ylabel(KNMI.attributes[att])
+    plt.legend()
+    plt.show()
+
+def boxplot_att(df, att, start=19010101, end=20991231):
+    '''Plots a boxplot for an attribute grouped by station. Statement 'by='STN' can easily be removed''' 
+    df.boxplot(by='STN', column=[att], grid=False)
+    plt.xlabel('stations')
+    plt.ylabel(att)
+    plt.legend()
+    plt.show()
+
 def covariance(atr1,atr2):
     filename = KNMI.PATH
     final_filename = filename[:filename.rindex('.')] + "_final.csv"
@@ -448,9 +538,19 @@ def main():
     reduced_filename = KNMI.PATH[:KNMI.PATH.rindex('.')] + ".csv"
     df = pd.read_csv(reduced_filename)
 
-    plot_measure_year(df, [])
-    plot_measure_month(df, [])
-    plot_measure_month(df, [210, 235, 240, 242], "FXX", end=19800101)
+    # tijdelijk: om te kijken of plot_att_month nog werkt
+    att = 'UN'
+    stations = []
+    plot_att_month(df, stations, att)
+    
+    # plot_measure_year(df, [])
+    # plot_measure_month(df, [])
+    plot_measure_month(df, [210, 215], "UN")
 
+    # voorbeeld boxplot
+    data = df.loc[(df.STN > 210) & (df.STN < 280)]
+    boxplot_att(data, 'UN')
 
-covariance('TG','TX')
+    covariance('TG','TX')
+
+main()
